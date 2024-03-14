51821e28804625ce0e8ba7983c8605bac682f04a
NancyM87 <nancym.sisa@gmail.com>
2024-03-13 21:35:37 -0500

    My first commit

---
+++ hola_mundo.java
@@ -0,0 +1,7 @@
+
+public class hola_mundo{
+    public static void main(String[] args)
+    {
+    System.out.print("Que mas veee!!");
+    }
+}
---
+++ sumar_numeros.java
@@ -0,0 +1,10 @@
+public class sumar_numeros{
+    public static void main(String[] args)
+    {
+        int num1=4;
+        int num2=5;
+        int suma=0;
+        suma= num1+num2;
+        System.out.println("La suma es:" +suma);
+    }
+}
